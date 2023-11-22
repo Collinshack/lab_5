ae56c8f8f8692fd454693dc8e214e975a4dfde51
Collins Awusa <awusacollins@gmail.com>
2023-11-22 14:47:23 +0300

    made changes

--- API_script.py
+++ API_script.py
@@ -1,12 +1,10 @@
 import requests
 import json
-from tabulate import tabulate 
+from tabulate import tabulate
 import datetime
-from tkinter import *
+from tkinter import Tk, Label, Button
 from PIL import Image, ImageTk
 import io
-
-
 
 # Задание 1
 def get_weather(city_name):
@@ -20,15 +18,12 @@ def get_weather(city_name):
     weather = f"{data_city['list'][0]['weather'][0]['main']}, {data_city['list'][0]['weather'][0]['description']}"
     humidity = data_city['list'][0]['main']['humidity']
     pressure = data_city['list'][0]['main']['pressure']
-    return weather, humidity, pressure
+    return city_name, weather, humidity, pressure
 
 
-
-city_name = 'Petersburg'
-info_tuple = get_weather(city_name)
-print(f"2. Информация о погоде в {city_name}\nОписание погоды: {info_tuple[0]}\nВлажность: {info_tuple[1]}\n"
-      f"Давление: {info_tuple[2]}\n")
-
+info_tuple = get_weather('Kursk')
+print(f"2. Информация о погоде в {info_tuple[0]}\nОписание погоды: {info_tuple[1]}\nВлажность: {info_tuple[2]}\n"
+      f"Давление: {info_tuple[3]}\n")
 
 
 # Задание 2
@@ -45,16 +40,13 @@ def json_parse(url):
     info_headers = ['валюта', 'название валюты', 'текущий курс обмена валюты', 'прошлый курс обмена валюты']
     info = []
     for currency in data['Valute']:
-        info.append([currency, data['Valute'][currency]['Name'],data['Valute'][currency]['Value'], data['Valute'][currency]['Previous']])
+        info.append([currency, data['Valute'][currency]['Name'], data['Valute'][currency]['Value'], data['Valute'][currency]['Previous']])
 
     return info, info_headers, date, len_currencies
 
 total_info = json_parse("https://www.cbr-xml-daily.ru/daily_json.js")
-print(f"3. Информация, полученная из API обменного курса cbr.ru\nдата: {total_info[2]}\nколичество считаемой валюты: {total_info[3]}"
+print(f"\n2. Информация, полученная из API обменного курса cbr.ru\nдата: {total_info[2]}\nколичество считаемой валюты: {total_info[3]}"
       f"\n{tabulate(total_info[0], headers=total_info[1])}")
-
-
-
 
 # Допзадание
 def fox_display():
@@ -81,11 +73,11 @@ def fox_display():
 
     fox_url = get_random_fox()
     fox_image = load_fox_image(fox_url)
-    
+
     fox_label = Label(window, image=fox_image)
     fox_label.place(x=0, y=0)
 
-    b = Button(window, text="Change Image", font=("Arial", 15), command=update_fox_image)
+    b = Button(window, text="Изменить изображение", font=("Arial", 15), command=update_fox_image)
     b.grid(row=5, column=3)
 
     window.mainloop()
